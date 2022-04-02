c2d6288e0cbfc0eed6bf927f1d3c831600580e26
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-02 16:48:31 +0600

    company logo add

--- src/component/company/Company.jsx
+++ src/component/company/Company.jsx
@@ -1,9 +1,18 @@
 import "./Company.css";
+import CompanyLogo from "./CompanyLogo";
 
 const Company = () => {
   return (
     <div className="companay container">
       <p>Over 32k+ software businesses growing with AR Shakir.</p>
+      <div className="companyLogo">
+        <CompanyLogo />
+        <CompanyLogo />
+        <CompanyLogo />
+        <CompanyLogo />
+        <CompanyLogo />
+        <CompanyLogo />
+      </div>
     </div>
   );
 };
---
+++ src/component/company/CompanyLogo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import companyLogo from "../../images/companyLogo.png";
+
+const CompanyLogo = () => {
+  return (
+    <>
+      <div className="companyLogo">
+        <img src={companyLogo} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default CompanyLogo;
